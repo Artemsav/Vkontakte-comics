b71eb88c9781f9555c5d4c157dd3c8b0ab2d7b5c
Artemsav <31589472+Artemsav@users.noreply.github.com>
2022-05-03 17:11:26 +0300

    derived separate function for image download

--- main.py
+++ main.py
@@ -16,8 +16,23 @@ def get_extention(url):
     return extention
 
 
-def download_random_comics():
-    pass
+def download_random_comics(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    fetch_response = response.json()
+    image_url = fetch_response['img']
+    ext = get_extention(image_url)
+    img_responce = requests.get(image_url)
+    img_responce.raise_for_status()
+    with open(f'image{ext}', 'wb') as file:
+        file.write(img_responce.content)
+
+
+def get_comment(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    fetch_response = response.json()
+    return fetch_response['alt']
 
 
 def wall_post():
@@ -30,15 +45,11 @@ if __name__ == '__main__':
     vk_group_id = os.getenv('VK_GROUP_ID')
     vk_app_id = os.getenv('VK_APP_CLIENT_ID')
     vk_api_version = '5.131'
-    url = 'https://xkcd.com/353/info.0.json'
-    response1 = requests.get(url)
-    response1.raise_for_status()
-    image_url = response1.json()['img']
-    ext = get_extention(image_url)
-    img_responce = requests.get(image_url)
-    img_responce.raise_for_status()
-    with open(f'image{ext}', 'wb') as file:
-        file.write(img_responce.content)
+    last_comics = 2614
+    rand_int = random.randint(1, last_comics)
+    url = f'https://xkcd.com/{rand_int}/info.0.json'
+    download_random_comics(url)
+    message = get_comment(url)
     upload_params = {
         'group_id': vk_group_id,
         'access_token': vk_access_token,
@@ -71,7 +82,6 @@ if __name__ == '__main__':
         save_wall_url = 'https://api.vk.com/method/wall.post'
         owner_id = fetch_save_response['response'][0]['owner_id']
         media_id = fetch_save_response['response'][0]['id']
-        message = response1.json()['alt']
         attachments = f'photo{owner_id}_{media_id}'
         wall_params = {
             'owner_id': f'-{vk_group_id}',
@@ -83,3 +93,4 @@ if __name__ == '__main__':
             'message': message
             }
         save_wall_response = requests.post(save_wall_url, params=wall_params)
+    os.remove('image.png')
